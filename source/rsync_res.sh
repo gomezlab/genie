1dc80162c043f6823edb17d055cd1fc9a694f491
kchen <kchen315@gmail.com>
2023-05-25 12:10:22 -0400

    add new preproc files

---
+++ source/rsync_res.sh
@@ -0,0 +1,7 @@
+rsync -avz --progress -e "ssh -i $HOME/.ssh/garm2_rsa" kchen315@longleaf.unc.edu:/nas/longleaf/home/kchen315/Documents/genie_crc/results/runs ../results/
+# rsync -avz --progress -e "ssh -i $HOME/.ssh/garm2_rsa" kchen315@longleaf.unc.edu:/nas/longleaf/home/kchen315/Documents/genie_crc/data/crc_reg_mut_cna_clin.csv ../data/crc_reg_mut_cna_clin.csv
+# rsync -avz --progress -e "ssh -i $HOME/.ssh/garm2_rsa" ../data/crc_reg_mut_cna_clin.csv kchen315@longleaf.unc.edu:/nas/longleaf/home/kchen315/Documents/genie_crc/data/crc_reg_mut_cna_clin.csv
+# rsync -avz --progress -e "ssh -i $HOME/.ssh/garm2_rsa" ../data/crc_ib_mut_cna_clin.csv kchen315@longleaf.unc.edu:/nas/longleaf/home/kchen315/Documents/genie_crc/data/crc_ib_mut_cna_clin.csv
+# rsync -avz --progress -e "ssh -i $HOME/.ssh/garm2_rsa" ../data/crc_egfr_mut_cna_clin.csv kchen315@longleaf.unc.edu:/nas/longleaf/home/kchen315/Documents/genie_crc/data/crc_egfr_mut_cna_clin.csv
+# rsync -avz --progress -e "ssh -i $HOME/.ssh/garm2_rsa" ../data/crc_vegf_mut_cna_clin.csv kchen315@longleaf.unc.edu:/nas/longleaf/home/kchen315/Documents/genie_crc/data/crc_vegf_mut_cna_clin.csv
+# rsync -avz --progress -e "ssh -i $HOME/.ssh/garm2_rsa" ../data/crc_folfox_mut_cna_clin.csv kchen315@longleaf.unc.edu:/nas/longleaf/home/kchen315/Documents/genie_crc/data/crc_folfox_mut_cna_clin.csv
